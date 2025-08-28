e217f18251a38a165d8606faa82f412815a8e5d8
Stanley Liu <sliu1993@hotmail.co.uk>
2025-08-28 04:02:23 -0400

    init

---
+++ server.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require 'json'
+require 'net/http'
+require 'uri'
+
+# Spotify MCP Server
+class SpotifyMCPServer
+  def initialize(access_token)
+    @access_token = access_token
+  end
+
+  def random_fact 
+    
+  end
+end
